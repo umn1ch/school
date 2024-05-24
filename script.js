f492517133b82fae9e33e3b455869b62cead1a86
Alex <sashok.umnov@gmail.com>
2024-05-24 14:23:59 +0300

    Stage 1
    -added footer
    -todo adaptive, animations

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// Get the button
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollToTopBtn.classList.add("visible");
+    } else {
+        scrollToTopBtn.classList.remove("visible");
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+scrollToTopBtn.addEventListener("click", function() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
+
 
 // SWIPER - можно не трогать, можно оставить толкьо инициализацию, как по кайфу
 document.addEventListener('DOMContentLoaded', function () {
